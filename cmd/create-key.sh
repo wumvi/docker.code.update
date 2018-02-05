1b6b10e621f56c43aeeb5ae65730b6b87594646f
Kozlenko Vitaliy <vk@wumvi.com>
2018-02-05 15:32:40 +0300

    fix connect

--- cmd/create-key.sh
+++ cmd/create-key.sh
@@ -9,6 +9,8 @@ RSA_PUB=`cat sshkey/login.pub`
 # Запрет ввода любых комманд, кроме указанных
 # echo 'command="/run.sh $SSH_ORIGINAL_COMMAND",no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty' $RSA_PUB > .ssh/authorized_keys
 
+echo $RSA_PUB > .ssh/authorized_keys
+
 chown -R $USER:$USER ./
 chmod 0600 sshkey/login
 chown root:root sshkey/login
